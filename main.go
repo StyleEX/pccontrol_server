96f161a6b20728ce3fffacab14b2336c92e805e7
Антон Владимиров <a.vladimirov@ideco.ru>
2015-05-05 21:04:16 +0500

    Use JSON RPC v2.0 over HTTP

--- main.go
+++ main.go
@@ -1,76 +1,57 @@
 package main
 
 import (
-	"bufio"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"encoding/json"
 	"fmt"
-
+	"github.com/gorilla/rpc/v2"
+	"github.com/gorilla/rpc/v2/json2"
 	"log"
-	"net"
+	"net/http"
 	"os/exec"
 )
 
-type Command struct {
-	Name string   `json:"name"`
-	Args []string `json:"args"`
+// TODO: Сервисы ержать в отдельном пакете.
+// VolumeService - управление звуком.
+type VolumeLevelArgs struct {
+	Level int
+}
+
+type VolumeService struct{}
+
+func (h *VolumeService) SetLevel(r *http.Request, args *VolumeLevelArgs, res *json2.EmptyResponse) error {
+	log.Printf("Set volume level %d%%", args.Level)
+	exec.Command("amixer", "sset", "'Master'", fmt.Sprintf("%d%%", args.Level)).Run()
+	return nil
+}
+
+type ShutdownArgs struct {
+	Minutes int
+}
+
+// SystemService - управление системой
+type SystemService struct{}
+
+func (h *SystemService) Shutdown(r *http.Request, args *ShutdownArgs, res *json2.EmptyResponse) error {
+	if args.Minutes > 0 {
+		log.Printf("Set shutdown after %d minutes", args.Minutes)
+		output, _ := exec.Command("shutdown", "-P", "-h", fmt.Sprintf("+%d", args.Minutes)).CombinedOutput()
+		log.Println(string(output[:]))
+	} else {
+		log.Println("Disable shutdown")
+		exec.Command("shutdown", "-c").Run()
+	}
+	return nil
 }
 
 func main() {
-	commands := make(chan Command)
-	go ListenTCP(commands, 9999)
+	s := rpc.NewServer()
+	s.RegisterCodec(json2.NewCodec(), "application/json")
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	// TODO: Регистрировать платформенно зависимые ханлеры.
+	s.RegisterService(new(VolumeService), "")
+	s.RegisterService(new(SystemService), "")
 
-	for {
-		select {
-		case command := <-commands:
-			if command.Name == "volume" {
-				exec.Command("amixer", "sset", "'Master'", fmt.Sprintf("%s%%", command.Args[0])).Run()
-			}
-			fmt.Println(command)
+	http.Handle("/rpc", s)
 
-		case <-signals:
-			return
-		}
-	}
-}
-
-func ListenTCP(output_stream chan Command, port int) error {
-	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
-	defer listener.Close()
-
-	if err != nil {
-		return err
-	}
-
-	for {
-		conn, _ := listener.Accept()
-
-		go func() {
-			// Receive message
-			scanner := bufio.NewScanner(conn)
-			for scanner.Scan() {
-				// Decode message
-				m := Command{}
-				if err := json.Unmarshal([]byte(scanner.Text()), &m); err != nil {
-					log.Println(err)
-					continue
-				}
-				// Output decode message
-				output_stream <- m
-			}
-
-			if err := scanner.Err(); err != nil {
-				log.Println(err)
-			}
-			conn.Close()
-		}()
-	}
-
-	return nil
+	// TODO: Использовать не HTTP, а держать TCP соединение.
+	http.ListenAndServe(":10000", nil)
 }
